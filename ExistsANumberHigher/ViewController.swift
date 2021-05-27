02456061bc73730370c2817e7fe7c3401764ba4a
Daniel Washington <danielvertigo@hotmail.com>
2021-05-27 10:43:13 -0300

    Files

--- ExistsANumberHigher/ViewController.swift
+++ ExistsANumberHigher/ViewController.swift
@@ -5,6 +5,19 @@
 //  Created by Daniel Washington Ignacio on 27/05/21.
 //
 
+
+/*
+ Write a function that returns true if there exists at least one number that is larger than or equal to n.
+
+ Examples
+ existsHigher([5, 3, 15, 22, 4], 10) ➞ true
+ existsHigher([1, 2, 3, 4, 5], 8) ➞ false
+ existsHigher([4, 3, 3, 3, 2, 2, 2], 4) ➞ true
+ existsHigher([], 5) ➞ false
+ */
+
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +25,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(existsHigher([5, 3, 15, 22, 4], 10))
+        print(existsHigher([1, 2, 3, 4, 5], 8))
+        print(existsHigher([4, 3, 3, 3, 2, 2, 2], 4))
+        print(existsHigher([], 5))
     }
-
+    func existsHigher(_ arr: [Int], _ n: Int) -> Bool {
+        for value in arr {
+            if n <= value {
+                return true
+            }
+        }
+        return false
+    }
 
 }
 
